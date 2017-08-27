76567c8e9c31d00349024ce9dcac65372f97c560
Chayanin <Chayanin>
2017-08-28 03:23:20 +0700

    sync templates to todos page

--- todos/views.py
+++ todos/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello World')
+    context = {
+        'name': 'ice'
+    }
+    return render(request, 'index.html', context)
