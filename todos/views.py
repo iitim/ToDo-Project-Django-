cf88020762a8dc86cdee8890d08e3d09bf74ea3d
Chayanin <Chayanin>
2017-08-28 04:28:30 +0700

    intialize 'add todo'

--- todos/views.py
+++ todos/views.py
@@ -18,3 +18,14 @@ def details(request, id):
         'todo': todo
     }
     return render(request, 'details.html', context)
+
+def add(request):
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+        return redirect('/todos')
+    else:
+        return render(request, 'add.html')
